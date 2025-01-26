09b6ec45483161e0a773e4fa577b6dc0b12d14a4
Adesh Kumar <adesh8061@gmail.com>
2025-01-27 00:18:15 +0530

    working on routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import UserLogin from "./pages/UserLogin";
+import UserSignup from "./pages/UserSignup";
+import CaptianLogin from "./pages/CaptianLogin";
+import CaptianSignup from "./pages/CaptianSignup";
 
 function App(){
-  return <div className="bg-red-700">App</div>
+  return <div >
+    <Routes>
+      <Route path ='/' element={<Home/>} />
+      <Route path ='/login' element={<UserLogin/>} />
+      <Route path ='/signup' element={<UserSignup/>} />
+      <Route path ='/captian-login' element={<CaptianLogin/>} />
+      <Route path ='/captian-signup' element={<CaptianSignup/>} />
+    </Routes>
+  </div>
 }
 
 export default App;
---
+++ Frontend/src/pages/CaptianLogin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CaptianLogin(){
+    return (
+        <div>CaptianLogin</div>
+    )
+}
+
+export default CaptianLogin;
